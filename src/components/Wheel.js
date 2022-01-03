fee58073f2805d58931583ea8ef12218d0f3805f
kareemIt <kareem.itani2@gmail.com>
2022-01-03 16:42:32 -0500

    still working on the wheel

--- src/components/Wheel.js
+++ src/components/Wheel.js
@@ -7,14 +7,26 @@ const Wheel = ({ array, landed }) => {
       <div className="mainbox">
         <div className="box">
           <div className="box1">
-            <span className="span1">1</span>
-            <span className="span2">2</span>
-            <span className="span3">3</span>
+            <span className="span1">
+              <strong>1</strong>
+            </span>
+            <span className="span2">
+              <strong>2</strong>
+            </span>
+            <span className="span3">
+              <strong>3</strong>
+            </span>
           </div>
           <div className="box2">
-            <span className="span1">4</span>
-            <span className="span2">5</span>
-            <span className="span3">6</span>
+            <span className="span1">
+              <strong>4</strong>
+            </span>
+            <span className="span2">
+              <strong>5</strong>
+            </span>
+            <span className="span3">
+              <strong>6</strong>
+            </span>
           </div>
         </div>
       </div>

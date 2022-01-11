00e9395e5e26a8b4e31d4f2d1c3c56a37f44746d
kareemIt <kareem.itani2@gmail.com>
2022-01-11 17:59:21 -0500

    started new way to display array

--- src/components/Wheel.js
+++ src/components/Wheel.js
@@ -2,6 +2,20 @@ import React from 'react';
 import './style.css';
 
 const Wheel = ({ array, landed }) => {
-  return <div></div>;
+  return (
+    <div>
+      <div>
+        {array.map((letter, index) => {
+          return (
+            <div className={'col ' + index} key={index}>
+              {letter.split('').map((letter, index) => (
+                <span className="letter-in">{letter}</span>
+              ))}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 export default Wheel;

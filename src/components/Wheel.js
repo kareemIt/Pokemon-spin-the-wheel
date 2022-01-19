72e82ca706c27b5f7d8f6b427f406fc2bb8aa10f
kareemIt <kareem.itani2@gmail.com>
2022-01-19 14:54:26 -0500

    added key to span

--- src/components/Wheel.js
+++ src/components/Wheel.js
@@ -9,7 +9,9 @@ const Wheel = ({ array, landed, wheelType }) => {
           return (
             <div className={'col' + index} key={index}>
               {letter.split('').map((letter, index) => (
-                <span className="letter">{letter}</span>
+                <span className="letter" key={index}>
+                  {letter}
+                </span>
               ))}
             </div>
           );
